a45001563493c446ffbcd7ea55787650b3404ab8
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-17 14:27:46 -0700

    add 2-print_alphabet.c script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (a = 65; a <= 90; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
