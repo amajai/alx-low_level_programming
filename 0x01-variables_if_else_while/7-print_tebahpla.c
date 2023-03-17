6297cbf20359063963873890edf6299140a9b7cd
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-17 15:02:09 -0700

    add 7-print_tebahpla script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
