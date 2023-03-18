2ddeaea36ac017489b4506aa709c5dcc32f207c8
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-18 12:22:14 -0700

    add 102-print_comb5.c script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+	int n4;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			for (n3 = n1; n3 <= 9; n3++)
+			{
+				for (n4 = n2 + 1; n4 <= 9; n4++)
+				{
+					if (n1 != 0 || n2 != 0 || n3 != 0 || n4 != 1)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(48 + n1);
+					putchar(48 + n2);
+					putchar(' ');
+					putchar(48 + n3);
+					putchar(48 + n4);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
