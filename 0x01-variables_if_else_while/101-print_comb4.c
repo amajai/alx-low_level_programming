993c37d7decad176321abf71865f9253f3383110
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-18 04:12:48 -0700

    add 101-print_comb4.c script

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+	int c;
+
+	c = 1;
+	for (n1 = 0; n1 <= 7; n1++)
+	{
+		for (n2 = c; n2 <= 8; n2++)
+		{
+			if (n1 != n2)
+			{
+				for (n3 = c + 1; n3 <= 9; n3++)
+				{
+					if (n1 != 0 || n2 != 1 || n3 != 2)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(48 + n1);
+					putchar(48 + n2);
+					putchar(48 + n3);
+				}
+
+			}
+		}
+		c += 1;
+	}
+	putchar('\n');
+	return (0);
+}
