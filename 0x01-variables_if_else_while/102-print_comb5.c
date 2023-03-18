4b13890aca26f572f3ee3e8195ac549689163130
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-18 12:32:37 -0700

    correct 102-print_comb5.c scriptx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,16 @@ int main(void)
 	int n2;
 	int n3;
 	int n4;
+	int c;
 
 	for (n1 = 0; n1 <= 9; n1++)
 	{
 		for (n2 = 0; n2 <= 9; n2++)
 		{
+			c = n2 + 1;
 			for (n3 = n1; n3 <= 9; n3++)
 			{
-				for (n4 = n2 + 1; n4 <= 9; n4++)
+				for (n4 = c; n4 <= 9; n4++)
 				{
 					if (n1 != 0 || n2 != 0 || n3 != 0 || n4 != 1)
 					{
@@ -31,6 +33,7 @@ int main(void)
 					putchar(48 + n3);
 					putchar(48 + n4);
 				}
+				c = 0;
 			}
 		}
 	}
