be57d1f96e1476df206c0ba7e1243e81a41d5407
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-21 05:15:56 -0700

    corect and change 0-putchar.c and adding main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints "_putchar", followed by a new line.
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	write(1, "_putchar\n", 9);
 	return (0);
 }
