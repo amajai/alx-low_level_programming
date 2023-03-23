e6fcdadbcdcf991133a5d78e82bc0e02b85bd364
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-03-23 02:25:15 -0700

    complete 2-mul.c program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The character to multiple.
+ * @b: The character to multiple.
+ *
+ * Return: Multiplied integer.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
