55036a058843788937b2e3cebe58e3b0a69f7676
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-09 09:25:14 -0700

    complete 0-whatsmyname.c program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name.
+ * @argc : args counts.
+ * @argv : args values in array
+ *
+ * Return : Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
