e5797d07449847e4f16fb9501261699a050d3bb5
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-09 07:07:38 -0700

    redo 0-puts_recursion.c program

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
 /**
+* print_chars - print one character from string.
+* @s : string.
+* @i : index of string.
+*
+* Return: always 1;
+*/
+int print_chars(unsigned int i, char *s)
+{
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return (1);
+	}
+	_putchar(s[i]);
+	return (print_chars(i + 1, s));
+}
+
+/**
  * _puts_recursion - prints a string.
  * @s :string to print.
- *i
+ *
  * Return: nothing.
  */
 void _puts_recursion(char *s)
 {
-	int c;
-	int i;
-
-	c = 0;
-	i = 0;
-	while (s[c] != '\0') /* count chars */
-	{
-		c += 1;
-	}
-
-	while (c >= 0) /* print every other characters */
-	{
-		_putchar(s[i]);
-		c -= 1;
-		i += 1;
-	}
-	_putchar('\n');
+	print_chars(0, s);
 }
