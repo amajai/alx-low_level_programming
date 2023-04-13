0693a143528ccf0bc6bd5990ea571f4fb2fb2795
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-13 10:15:05 -0700

    correct 00-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (mem);
 	}
 
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < old_size; i++)
 	{
 		((char *) mem)[i] = ((char *) ptr)[i];
 	}
