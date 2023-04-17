de0ff38070b1474a3e944a2eed592c7d58733ff5
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-17 14:05:36 -0700

    complete 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs (free allocated memory where dog data is saved).
+ * @d: struct type.
+ *
+ * Return: nothing.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
