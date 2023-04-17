162614bc3bd3ee68f846b6ba4684047961f6ffc6
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-17 13:52:11 -0700

    correct 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
+/**
+ * _strlen - counter number of characters in string
+ * @s: string
+ *
+ * Return: number of characters;
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 /**
  * new_dog - creates a new dog.
  * @name: dog name string.
@@ -11,13 +29,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, name_len, owner_len;
 	dog_t *new_dog_mem;
 
 	new_dog_mem = malloc(sizeof(struct dog));
 	if (new_dog_mem == NULL)
 		return (NULL);
-	new_dog_mem->name = name;
+	name_len = _strlen(name) + 1;
+	new_dog_mem->name = malloc(sizeof(char) * name_len);
+	if (new_dog_mem->name == NULL)
+	{
+		free(new_dog_mem);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+	{
+		new_dog_mem->name[i] = name[i];
+	}
+
+	owner_len = _strlen(owner) + 1;
+	new_dog_mem->owner = malloc(sizeof(char) * owner_len);
+	if (new_dog_mem->owner == NULL)
+	{
+		free(new_dog_mem->name);
+		free(new_dog_mem);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+	{
+		new_dog_mem->owner[i] = owner[i];
+	}
 	new_dog_mem->age = age;
-	new_dog_mem->owner = owner;
 	return (new_dog_mem);
 }
