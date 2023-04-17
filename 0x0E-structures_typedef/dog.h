69fa4345042bf4fedc9bb71cb5f053f44497f904
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-17 07:38:31 -0700

    complete 0x0E-structures_typedef/dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct data type with 3 elements
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: a struct data type with 3 attributes for a dog owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
