3368f825d18e5f0b864e68be7737761e4ea22d13
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-18 03:25:20 -0700

    correct 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,6 +19,8 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
 		isMatch = wildcmp(s1, s2 + 1);
 		if (isMatch == 1)
 			return (1);
