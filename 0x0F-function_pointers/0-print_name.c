d18aaa653680abeca847c9cfc70a635d0b5295ac
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-19 03:18:02 -0700

    complete 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints name.
+ * @name: name string.
+ * @f: a function.
+ *
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
