d84d8587de5a4c51a91711417f360f5279df247a
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-19 07:32:22 -0700

    correct 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, result;
 
+	if (array == NULL || cmp == NULL)
+		return;
+
 	if (size < 0)
 		return (-1);
 	for (i = 0; i < size; i++)
