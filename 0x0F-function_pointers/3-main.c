3b82d30c3e703f1e2730272e43eda8ea9d06f310
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-19 11:51:59 -0700

    correct task 3 0x0F-function_pointers:x

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,14 +22,17 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (*argv[2] == '\0' || argv[2] == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	get_op = get_op_func(argv[2]);
 	if (get_op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (!isdigit(*argv[1]) || !isdigit(*argv[3]))
-		return (1);
 	result = get_op(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
 	return (0);
