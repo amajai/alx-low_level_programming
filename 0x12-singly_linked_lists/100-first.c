7105db7dad603935828e0924e0f8ad598e0c5bfe
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-04-27 17:06:42 -0700

    complete 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first - execute function before main function.
+ *
+ * Return: nothing
+ */
+void __attribute__((constructor))first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
