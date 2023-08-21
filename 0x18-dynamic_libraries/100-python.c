a9a6edefa813bdf7b7277a1cdcc90af81dc2d09a
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-08-21 15:11:23 +0100

    init 100-operations.so task

---
+++ 0x18-dynamic_libraries/100-python.c
@@ -0,0 +1,70 @@
+#include "Python.h"
+#include "main.h"
+
+/**
+ * add - sum of the first and second integer.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: value calculated.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - difference of the first and second integer.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: value calculated.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication of the first and second integer.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: value calculated.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division of the first and second integer.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: value calculated.
+ * if b is 0, return status 100
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * mod - remainder for division of the first and second integer.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: value calculated.
+ * if b is 0, return status 100;
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
