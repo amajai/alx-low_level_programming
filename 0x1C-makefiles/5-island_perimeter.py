d7b1430596d42b4e46c8ffb2cb1a868b3024b0a8
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-10-25 15:05:00 +0100

    done 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module is for getting an island perimeter in a grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == 1:
+                if y - 1 >= 0 and grid[y-1][x] == 0:
+                    perimeter += 1
+                if x + 1 < len(grid[y]) and grid[y][x+1] == 0:
+                    perimeter += 1
+                if x - 1 >= 0 and grid[y][x-1] == 0:
+                    perimeter += 1
+            else:
+                if y - 1 >= 0 and grid[y-1][x] == 1:
+                    perimeter += 1
+    return perimeter
