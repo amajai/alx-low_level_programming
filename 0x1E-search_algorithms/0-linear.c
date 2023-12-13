979b7d33ab68653c80359569a35bede9faa5f8b7
Abdulmajeed Isa <aabdulmajeed.isa@gmail.com>
2023-12-13 15:11:18 +0100

    change 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,6 +15,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
